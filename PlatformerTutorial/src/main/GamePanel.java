24455e71c6eaa6f1213a0fec234bc0943f349905
SLajuwomi <stephenlajuwomi@gmail.com>
2024-08-02 12:53:09 -0500

    Used Image IO to import image

--- PlatformerTutorial/src/main/GamePanel.java
+++ PlatformerTutorial/src/main/GamePanel.java
@@ -1,12 +1,12 @@
 package main;
 
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.util.ArrayList;
-import java.util.Random;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.*;
 
 import javax.swing.JPanel;
 
@@ -17,17 +17,29 @@ public class GamePanel extends JPanel {
 
 	private MouseInputs mouseInputs;
 	private float xDelta = 100, yDelta = 100;
-	private float xDir = 1f, yDir = 1f;
-	private Color color = new Color(150, 20, 90);
-	private Random random;
+	private BufferedImage img;
 
 	public GamePanel() {
-		random = new Random();
+
 		mouseInputs = new MouseInputs(this);
+
+		importImg();
+
 		setPanelSize();
 		addKeyListener(new KeyboardInputs(this));
 		addMouseListener(mouseInputs);
 		addMouseMotionListener(mouseInputs);
+
+	}
+
+	private void importImg() {
+		InputStream is = getClass().getResourceAsStream("/player_sprites.png");
+
+		try {
+			img = ImageIO.read(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
@@ -53,44 +65,11 @@ public class GamePanel extends JPanel {
 		this.yDelta = y;
 	}
 
-	public void spawnRect(int x, int y) {
-		rects.add(new MyRect(x, y));
-	}
-
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
-		updateRectangle();
+		// g.drawImage(null, x, y, null);
 
-		g.setColor(color);
-		g.fillRect((int) xDelta, (int) yDelta, 200, 50);
-
-	}
-
-	private void updateRectangle() {
-		xDelta += xDir;
-		if (xDelta > 400 || xDelta < 0) {
-			xDir *= -1;
-			color = getRndColor();
-		}
-
-		yDelta += yDir;
-		if (yDelta > 400 || yDelta < 0) {
-			yDir *= -1;
-			color = getRndColor();
-		}
-	}
-
-	private Color getRndColor() {
-		int r = random.nextInt(255);
-		int g = random.nextInt(255);
-		int b = random.nextInt(255);
-
-		return new Color(r, g, b);
-	}
-
-	private Color newColor() {
-		return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
 	}
 
 }

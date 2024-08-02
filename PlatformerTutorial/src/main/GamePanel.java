aba24c77084d83fe1380fa4499d8c2c6a6e18149
SLajuwomi <stephenlajuwomi@gmail.com>
2024-08-02 12:06:29 -0500

    set size of jFrame according to the panel to remove borders from being included with size

--- PlatformerTutorial/src/main/GamePanel.java
+++ PlatformerTutorial/src/main/GamePanel.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -20,16 +21,21 @@ public class GamePanel extends JPanel {
 	private Color color = new Color(150, 20, 90);
 	private Random random;
 
-	// Temp
-	private ArrayList<MyRect> rects = new ArrayList<>();
-
 	public GamePanel() {
 		random = new Random();
 		mouseInputs = new MouseInputs(this);
+		setPanelSize();
 		addKeyListener(new KeyboardInputs(this));
 		addMouseListener(mouseInputs);
 		addMouseMotionListener(mouseInputs);
 
+	}
+
+	private void setPanelSize() {
+		Dimension size = new Dimension(1280, 800);
+		setMinimumSize(size);
+		setPreferredSize(size);
+		setMaximumSize(size);
 	}
 
 	public void changeXDelta(int value) {
@@ -53,12 +59,6 @@ public class GamePanel extends JPanel {
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-
-		// Temp Rects
-		for (MyRect rect : rects) {
-			rect.updateRect();
-			rect.draw(g);
-		}
 
 		updateRectangle();
 
@@ -89,44 +89,8 @@ public class GamePanel extends JPanel {
 		return new Color(r, g, b);
 	}
 
-	// Temp
-	public class MyRect {
-		int x, y, w, h;
-		int xDir = 1, yDir = 1;
-		Color color;
-
-		public MyRect(int x, int y) {
-			this.x = x;
-			this.y = y;
-			w = random.nextInt(50);
-			h = w;
-			color = newColor();
-		}
-
-		public void updateRect() {
-			this.x += xDir;
-			this.y += yDir;
-
-			if ((x + w) > 400 || x < 0) {
-				xDir *= -1;
-				color = newColor();
-			}
-			if ((y + h) > 400 || y < 0) {
-				yDir *= -1;
-				color = newColor();
-			}
-
-		}
-
-		private Color newColor() {
-			return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
-		}
-
-		public void draw(Graphics g) {
-			g.setColor(color);
-			g.fillRect(x, y, w, h);
-		}
-
+	private Color newColor() {
+		return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
 	}
 
 }
